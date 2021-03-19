5280aad117e26446635f1a6d980f3a3bf7479eb7
Arthuro12 <77574113+Arthuro12@users.noreply.github.com>
2021-03-19 10:00:20 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ProgDemy</title>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style1.css" />
+    </head>
+
+    <body>
+        <header>         
+            <div class="header__logo">
+              <h1><a href="#">ProgDemy</a></h1>
+            </div>
+            <nav>               
+                <ul>
+                    <li style="border-left: 0; padding-left: 0; margin-left: 0;"><a href="#">STARTSEITE</a></li>
+                    <li style="border-left: 1px solid black; padding-left: 1em; margin-left: 1em;"><a href="#">KURSE</a></li>
+                    <li style="border-left: 1px solid black; padding-left: 1em; margin-left: 1em;"><a href="#">ANMELDUNG</a></li>
+                    <li style="border-left: 1px solid black; padding-left: 1em; margin-left: 1em;"><a href="#">REGISTRIERUNG</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <div class="banner">
+            <div class="banner__description">
+               <h1 class="banner__h1">Hier treffen sich <span class="banner__word1">Learning</span> und <span class="banner__word2">Doing</span>!</h1>
+               <br />
+               <p class="banner__p">Aquae ad his navigerum Hierosolymis iuris domitis multiplicium nusquam in.</p><br />
+            </div>
+            <div class="banner__btn">
+                <a href="#">Mehr wissen</a>
+            </div>
+        </div>
+
+        <section class="part1">
+            <div class="part1-description">
+               <h1>Kurse wie keine andere</h1><br />
+               <p>Apollinares quaedam Apollinares in constaret post lapide actitata<br />
+               filius ab multiplices post ambigerentur non igitur lapide levius<br /> quarto multorum pervenissent.</p><br />
+               <a href="#">Mehr wissen</a>
+            </div>
+            <div class="part1__img">
+                <img src="Images/class1.jpeg" alt="class" height=400 width=600 />
+            </div>
+        </section>
+
+        <section class="part2">
+            <div class="part2__title">
+                <h2>Unsere Profs geben das beste für euch</h2>
+            </div>
+            <div class="part2-description">           
+               <p>
+                    Quibus occurrere bene pertinax miles explicatis ordinibus parans hastisque feriens 
+                    scuta qui habitus iram pugnantium concitat et dolorem proximos iam <br >gestu terrebat sed
+                    eum in certamen alacriter consurgentem revocavere ductores rati intempestivum anceps
+                    subire certamen cum haut longe muri distarent,<br > quorum tutela se curitas poterat in solido locari cunctorum.
+               </p><br >
+               <div class="part2-description__btn">
+                   <a href="#">Starten Sie eine Ausbildung</a>
+                </div>
+            </div>
+        </section>
+
+        <section class="part3">
+            <div class="part3__title">
+               <h2>Was unsere Studenten denken</h2>
+            </div>
+            <div class="both">
+                <div class="firstOfBoth">
+                    <div class="firstOfBoth__img">
+                        <img src="Images/student1.jpeg" alt="student2" height=200 widht=200 />
+                    </div>
+                    <div class="firstOfBoth__description">
+                        <h4>Vorname Nachname</h4>
+                        <cite>
+                            Web-Enntwickler-Junior
+                        </cite>
+                        <p>
+                        " Apollinares quaedam Apollinares in constaret post lapide<br /> actitata filius ab multiplices post 
+                         ambigerentur non igitur<br /> lapide levius quarto multorum pervenissent. "
+                        </p>
+                    </div>
+                </div>
+                <div class="secondOfBoth">
+                    <div class="secondOfBoth__img">
+                        <img src="Images/student2.jpeg" alt="student2" height=200 widht=200 />
+                    </div>
+                    <div class="secondOfBoth__description">
+                        <h4>Vorname Nachname</h4>
+                        <cite>
+                           Web-Enntwicklerin-Junior
+                        </cite>
+                        <p>
+                          " Apollinares quaedam Apollinares in constaret post lapide<br /> actitata filius ab multiplices post 
+                           ambigerentur non igitur<br /> lapide levius quarto multorum pervenissent. "
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer>
+           
+            <div class="footer__child1">
+               <span>
+                   <p>ProgDemy</p>
+               </span>
+               <div>
+               <ul>
+                   <li><a href="#">Die Firma</a></li>
+                   <li><a href="#">Forum</a></li>
+                   <li><a href="#">Blog</a></li>
+                </ul>
+               </div>
+            </div>
+
+            <div class="footer__child2">
+                <a href="#">Kontakt</a>
+            </div>
+
+            <div class="footer__child3">
+                <h3>Unsere Netzwerke</h3>
+                <div class="icon">
+                    <div><img src="Images/facebook.png" alt="facebook" height=40 widht=40/></div>
+                    <div><img src="Images/instagram.png" alt="instagram" height=40 widht=40/></div>
+                    <div><img src="Images/skype.png" alt="skype" height=40 widht=40/></div>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
